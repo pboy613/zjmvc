770ea7cc3c2c001857fccc5021bc06a0da2ca689
jun <jun@junPC>
2015-07-20 20:16:52 +0800

    zj

---
+++ src/main/java/zjmvc/entity/User.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2014 springside.github.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *******************************************************************************/
+package zjmvc.entity;
+
+import java.util.Date;
+
+public class User extends IdEntity {
+	private String loginName;
+	private String name;
+	private String plainPassword;
+	private String password;
+	private String salt;
+	private String roles;
+	private Date registerDate;
+
+	public User() {
+	}
+
+	public User(Long id) {
+		this.id = id;
+	}
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// 不持久化到数据库，也不显示在Restful接口的属性.
+	public String getPlainPassword() {
+		return plainPassword;
+	}
+
+	public void setPlainPassword(String plainPassword) {
+		this.plainPassword = plainPassword;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	
+}
---
+++ src/test/java/zjmvc/QuickStartServer.java
@@ -0,0 +1,38 @@
+package zjmvc;
+
+import org.eclipse.jetty.server.Server;
+import org.springside.modules.test.jetty.JettyFactory;
+import org.springside.modules.test.spring.Profiles;
+
+
+public class QuickStartServer {
+	public static final int PORT = 8080;
+	public static final String CONTEXT = "/zjmvc";
+	public static final String[] TLD_JAR_NAMES = new String[] { "spring-webmvc", "shiro-web", "springside-core" };
+
+	public static void main(String[] args) throws Exception {
+		// 设定Spring的profile
+		Profiles.setProfileAsSystemProperty(Profiles.DEVELOPMENT);
+
+		// 启动Jetty
+		Server server = JettyFactory.createServerInSource(PORT, CONTEXT);
+		JettyFactory.setTldJarNames(server, TLD_JAR_NAMES);
+
+		try {
+			server.start();
+			System.out.println("[INFO] Server running at http://localhost:" + PORT + CONTEXT);
+			System.out.println("[HINT] Hit Enter to reload the application quickly");
+
+			// 等待用户输入回车重载应用.
+			while (true) {
+				char c = (char) System.in.read();
+				if (c == '\n') {
+					JettyFactory.reloadContext(server);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+}
